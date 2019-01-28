04f4816af4dbe004a028aeef606c2bcbc706ca87
Yosep Ferdianto <46852674+yosefdn@users.noreply.github.com>
2019-01-28 13:31:23 +0700

    upload2

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  public textJudul = 'Test Praktek';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
---
+++ src/app/photos.model.ts
@@ -0,0 +1,7 @@
+export interface IPhotos {
+    albumId: number;
+    id: string;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+}
